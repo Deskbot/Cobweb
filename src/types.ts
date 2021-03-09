54182cf3577750f34331ba2c161688ec71d5dab7
Thomas Richards <Deskbot@users.noreply.github.com>
2021-03-09 22:07:25 +0000

    Reformat

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,27 @@
 import { IncomingMessage, ServerResponse } from "http";
 
-export interface RequestHandler<M extends Middleware<Req, C>, Req = IncomingMessage, Res = ServerResponse, C = any> {
+export interface RequestHandler<
+    M extends Middleware<Req, C>,
+    Req = IncomingMessage,
+    Res = ServerResponse,
+    C = any
+> {
     (req: Req, res: Res, middleware: M): void | Promise<void>
 }
 
-export interface RequestSideEffect<M extends Middleware<Req, C>, Req = IncomingMessage, C = any> {
+export interface RequestSideEffect<
+    M extends Middleware<Req, C>,
+    Req = IncomingMessage,
+    C = any
+> {
     (req: Req, middleware: M): void;
 }
 
-export interface RequestPredicate<M extends Middleware<Req, C>, Req = IncomingMessage, C = any> {
+export interface RequestPredicate<
+    M extends Middleware<Req, C>,
+    Req = IncomingMessage,
+    C = any
+> {
     (req: Req, middleware: M): boolean | Promise<boolean>;
 }
 
@@ -20,16 +33,33 @@ export interface SpyCatch<Req = IncomingMessage> {
     catch?: (error: any, req: Req) => void;
 }
 
-export interface Endpoint<M extends Middleware<Req, C>, Req = IncomingMessage, Res = ServerResponse, C = any> extends EndpointCatch<Req, Res> {
+export interface Endpoint<
+    M extends Middleware<Req, C>,
+    Req = IncomingMessage,
+    Res = ServerResponse,
+    C = any
+> extends EndpointCatch<Req, Res>
+{
     when: RequestPredicate<M, Req>;
     do: RequestHandler<M, Req, Res>;
 }
 
-export interface FallbackEndpoint<M extends Middleware<Req, C>, Req = IncomingMessage, Res = ServerResponse, C = any> extends EndpointCatch<Req, Res> {
+export interface FallbackEndpoint<
+    M extends Middleware<Req, C>,
+    Req = IncomingMessage,
+    Res = ServerResponse,
+    C = any
+> extends EndpointCatch<Req, Res>
+{
     do: RequestHandler<M, Req, Res>;
 }
 
-export interface Spy<M extends Middleware<Req, C>, Req = IncomingMessage, C = any> extends SpyCatch<Req> {
+export interface Spy<
+    M extends Middleware<Req, C>,
+    Req = IncomingMessage,
+    C = any
+> extends SpyCatch<Req>
+{
     when: RequestPredicate<M, Req>;
     do: RequestSideEffect<M, Req>;
 }
@@ -41,9 +71,17 @@ export type MiddlewareSpec<
 >
     = Record<K, (req: Req, context: C) => any>;
 
-export type Middleware<C, Req, Spec extends MiddlewareSpec<keyof any, Req, C> = any> = {
+export type Middleware<
+    C,
+    Req,
+    Spec extends MiddlewareSpec<keyof any, Req, C> = any
+> = {
     [N in keyof Spec]: () => ReturnType<Spec[N]>
 };
 
-export type Quelaag<C, M extends Middleware<Req, C>, Req = IncomingMessage> =
-    (req: Req, context?: C) => M
+export type Quelaag<
+    C,
+    M extends Middleware<Req, C>,
+    Req = IncomingMessage
+> =
+    (req: Req, context?: C) => M;
