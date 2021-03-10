7e98441e6401d42ff77ed6157f202bf2d13f167d
Thomas Richards <Deskbot@users.noreply.github.com>
2021-03-10 18:20:34 +0000

    Make tests separate

--- test/src/quelaag/subquelaag.ts
+++ test/src/quelaag/subquelaag.ts
@@ -19,68 +19,74 @@ function setup(callback: () => void) {
 
 export const subquelaagTests: Test[] = [
 {
-    name: "Context as Quelaag",
-    cases: 12,
+    name: "Super Quelaag called first",
+    cases: 4,
     run: ({ test }) => {
         let count = 0;
 
         let { makeMiddleware1, makeMiddleware2 } = setup(() => count += 1);
 
-        {
-            count = 0;
+        const mid1 = makeMiddleware1("", undefined);
+        const mid2 = makeMiddleware2("", mid1);
 
-            const mid1 = makeMiddleware1("", undefined);
-            const mid2 = makeMiddleware2("", mid1);
+        // call mid1, then mid2
+        mid1.func();
+        test(count === 1);
+        mid1.func();
+        test(count === 1);
 
-            // call mid1, then mid2
-            mid1.func();
-            test(count === 1);
-            mid1.func();
-            test(count === 1);
-
-            mid2.func();
-            test(count === 1);
-            mid2.func();
-            test(count === 1);
-        }
-
-        {
-            count = 0;
-
-            const mid1 = makeMiddleware1("", undefined);
-            const mid2 = makeMiddleware2("", mid1);
-
-            // call mid2, then mid1
-            mid2.func();
-            test(count === 1);
-            mid2.func();
-            test(count === 1);
-
-            mid1.func();
-            test(count === 1);
-            mid1.func();
-            test(count === 1);
-        }
-
-        {
-            count = 0;
-
-            const mid1 = makeMiddleware1("", undefined);
-            const mid2 = makeMiddleware2("", mid1);
-
-            // mix order
-            mid1.func();
-            test(count === 1);
-
-            mid2.func();
-            test(count === 1);
-
-            mid1.func();
-            test(count === 1);
-
-            mid2.func();
-            test(count === 1);
-        }
+        mid2.func();
+        test(count === 1);
+        mid2.func();
+        test(count === 1);
     }
-}
+},
+{
+    name: "Sub Quelaag called first",
+    cases: 4,
+    run: ({ test }) => {
+        let count = 0;
+
+        let { makeMiddleware1, makeMiddleware2 } = setup(() => count += 1);
+
+        const mid1 = makeMiddleware1("", undefined);
+        const mid2 = makeMiddleware2("", mid1);
+
+        // call mid2, then mid1
+        mid2.func();
+        test(count === 1);
+        mid2.func();
+        test(count === 1);
+
+        mid1.func();
+        test(count === 1);
+        mid1.func();
+        test(count === 1);
+    }
+},
+{
+    name: "Super and Sub Quelaag calls jumbled up",
+    cases: 4,
+    run: ({ test }) => {
+        let count = 0;
+
+        let { makeMiddleware1, makeMiddleware2 } = setup(() => count += 1);
+
+        const mid1 = makeMiddleware1("", undefined);
+        const mid2 = makeMiddleware2("", mid1);
+
+        // mix order
+        mid1.func();
+        test(count === 1);
+
+        mid2.func();
+        test(count === 1);
+
+        mid1.func();
+        test(count === 1);
+
+        mid2.func();
+        test(count === 1);
+    }
+},
 ];
