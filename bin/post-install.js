2d8a7990eeda5427208136c0c1f67d9664469d14
Thomas Richards <Deskbot@users.noreply.github.com>
2021-06-28 11:40:54 +0100

    fix: ensure post-install script runs

---
+++ bin/post-install.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log("Warning: breaking API changes since 0.0.9")
+console.log("Check README for differences. Upgrading shouldn't be too difficult.")
+console.log()
--- post-install.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-console.log("Warning: breaking API changes since 0.0.9")
-console.log("Check README for differences. Upgrading shouldn't be difficult.")
